edd950058c16d8fea3819cc725e415856828f111
Kirill <efremenkov1999@gmail.com>
2020-08-31 01:23:43 +0300

    add slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,25 @@
-var sum = 5;
-console.log('sum= ', sum);
+document.addEventListener('DOMContentLoaded', () => {
+    var mySwiper = new Swiper('.actors-slider', {
+        loop: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        slidesPerView: 1,
+        renderFraction: function (currentClass, totalClass) {
+          return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
+        }
+      });
+      var myThirdSwiper = new Swiper('.contacts-slider', {
+        loop: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        slidesPerView: 1
+      });
+});
