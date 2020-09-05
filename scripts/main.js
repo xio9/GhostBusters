4f9fc473a274e8da3f0a5f5b34fa61f4a200e88e
Kirill <efremenkov1999@gmail.com>
2020-09-05 15:43:07 +0300

    add swiper buttons

--- scripts/main.js
+++ scripts/main.js
@@ -5,21 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev'
         },
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'fraction'
-        },
         slidesPerView: 1,
-        renderFraction: function (currentClass, totalClass) {
-          return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
-        }
-      });
-      var myThirdSwiper = new Swiper('.contacts-slider', {
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        },
-        slidesPerView: 1
       });
 });
